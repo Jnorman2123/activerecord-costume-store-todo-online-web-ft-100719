6d21fa29338475f6fd5c2bdeada4d3e32cbce5bd
Jonathan Norman <jnorman2123@gmail.com>
2019-11-12 15:53:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
   def change
-    create_table haunted_houses do |house|
+    create_table :haunted_houses do |house|
       house.string :name
       house.string :location
       house.string :theme
